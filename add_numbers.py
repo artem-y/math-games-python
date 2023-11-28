f19cbeab6f6b4bea1f32b321213c5217d54adf1e
Artem Yelizarov <52959979+artem-y@users.noreply.github.com>
2023-11-28 12:06:24 +0200

    Improve naming in add numbers game

--- add_numbers.py
+++ add_numbers.py
@@ -4,13 +4,13 @@ import time
 def generate_random_number():
     return random.randint(1000, 9999)
 
-def main():
-    num1 = generate_random_number()
-    num2 = generate_random_number()
-    
-    start_time = time.time()
+def add_numbers():
+    first_number = generate_random_number()
+    second_number = generate_random_number()
 
-    question = f"{num1} + {num2} = "
+    question = f"{first_number} + {second_number} = "
+
+    start_time = time.time()
     user_answer = input(question)
 
     try:
@@ -20,14 +20,14 @@ def main():
         return
 
     end_time = time.time()
-    elapsed_time = end_time - start_time
+    time_passed = end_time - start_time
     
-    actual_sum = num1 + num2
+    sum = first_number + second_number
     
-    if user_answer == actual_sum:
-        print(f"Correct! It took you {elapsed_time:.2f} seconds.")
+    if user_answer == sum:
+        print(f"Correct! It took you {time_passed:.2f} seconds.")
     else:
-        print(f"Wrong answer! Correct answer is {actual_sum}.")
+        print(f"Wrong! Correct answer is {sum}.")
 
 if __name__ == "__main__":
-    main()
+    add_numbers()
